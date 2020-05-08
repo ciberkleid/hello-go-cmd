4827958ecd887065e36fc914f93f8561e4694cb3
Cora Iberkleid <ciberkleid@gmail.com>
2020-05-08 09:23:08 -0400

    add env var to support paketo builds

--- hello-box/hello-box.go
+++ hello-box/hello-box.go
@@ -8,18 +8,30 @@ import (
 
 func main () {
 
-    value := "-h"
+    value := ""
     if len(os.Args) > 1 {
         value = os.Args[1]
+    } else {
+        value = os.Getenv("HELLO_BOX_ARG")
+    }
+    if len(value) == 0 {
+        value = "-h"
     }
 
     if value == "-h" || value == "--help" {
-        fmt.Println("USAGE: Provide a string and we'll say hello!")
+        fmt.Println("USAGE: Provide a string and we'll say hello! [can use env var HELLO_BOX_ARG]")
     } else {
         output := "Hello, " + value + "!"
         output, _ = strutil.DrawCustomBox(output, 40, strutil.Center, strutil.SimpleBox9Slice(), "\n")
         fmt.Println(output)
     }
+
+    //For builds with CNB/Paketo:
+    //    Using base run-image, two options (command overrides env var):
+    //        docker run -e HELLO_BOX_ARG=world hello:base "hello-box sunshine"
+    //    Using tiny run image, use:
+    //        docker run -e HELLO_BOX_ARG=world hello:tiny
+
 
 }
 
