8c6c163c7abe0130eb529dff894c6673f4276cb8
Cora Iberkleid <ciberkleid@gmail.com>
2020-05-09 16:04:55 -0400

    simplify to single hello app

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module example.com/hello-box
+module example.com/hello
 
 go 1.14
 
--- hello.go
+++ hello.go
@@ -12,14 +12,14 @@ func main () {
     if len(os.Args) > 1 {
         value = os.Args[1]
     } else {
-        value = os.Getenv("HELLO_BOX_ARG")
+        value = os.Getenv("HELLO_ARG")
     }
     if len(value) == 0 {
         value = "-h"
     }
 
     if value == "-h" || value == "--help" {
-        fmt.Println("USAGE: Provide a string and we'll say hello! [can use env var HELLO_BOX_ARG]")
+        fmt.Println("USAGE: Provide a string and we'll say hello!\n       [Command-line arg or env var HELLO_ARG]")
     } else {
         output := "Hello, " + value + "!"
         output, _ = strutil.DrawCustomBox(output, 40, strutil.Center, strutil.SimpleBox9Slice(), "\n")
@@ -28,7 +28,7 @@ func main () {
 
     //For builds with CNB/Paketo:
     //    Using base run-image, two options (command overrides env var):
-    //        docker run -e HELLO_BOX_ARG=world hello:base "hello-box sunshine"
+    //        docker run -e HELLO_ARG=world hello:base "hello sunshine"
     //    Using tiny run image, use:
     //        docker run -e HELLO_BOX_ARG=world hello:tiny
 
